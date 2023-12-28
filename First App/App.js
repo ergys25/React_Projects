f15e9603a601d103c58d9452cf6dc7852cebae6e
Ergys Hila <ergysmania@gmail.com>
2023-12-27 21:47:47 -0500

    Refactor function to improve performance

---
+++ First App/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function App() {
+    const [ advice, setAdvice ] = useState("");
+
+    async function getAdvice()
+    {
+        const response = await fetch("https://api.adviceslip.com/advice");
+        const data = await response.json();
+        setAdvice(data.slip.advice);
+    }
+
+    return (
+        <div>
+            <h1>Advice App</h1>
+            <button onClick={getAdvice}>Get Advice</button>
+            <h2>{advice}</h2>
+        </div>
+    );
+
+}   // end App
+
