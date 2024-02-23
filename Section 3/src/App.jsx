c79cf75f5b3c844c53e0bdde6bb875ac765295ec
Ergys Hila <ergysmania@gmail.com>
2024-02-23 17:19:08 -0500

    Section 3

--- Section 3/src/App.jsx
+++ Section 3/src/App.jsx
@@ -1,38 +1,8 @@
 import reactImg from './assets/react-core-concepts.png'
 import {CORE_CONCEPTS} from './data';
-
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept(props) {
-    return (<li>
-            <img src={props.image} alt={props.title}/>
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-        </li>
-
-    )
-}
-
-function Header() {
-
-    return (<header>
-        <img src={reactImg} alt="Stylized atom"/>
-        <h1>React Essentials</h1>
-        <p>
-            {reactDescription[getRandomInt(reactDescription.length)]} React concepts you will need for almost any
-            app you are
-            going to build!
-        </p>
-    </header>)
-
-
-}
-
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
     return (<div>
@@ -42,18 +12,25 @@ function App() {
             <section id="core-concepts">
                 <h2>
                     <ul>
-
-
                         <CoreConcept {...CORE_CONCEPTS[0]}></CoreConcept>
                         <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
-
                         <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
                         <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
-
-
                     </ul>
                 </h2>
             </section>
+            <section id="examples">
+                <h2>Examples</h2>
+                <menu>
+                    <TabButton>Javas</TabButton>
+                    <TabButton>JSX</TabButton>
+                    <TabButton>Props</TabButton>
+                    <TabButton>State</TabButton>
+                </menu>
+
+
+            </section>
+
         </main>
 
         <main>
