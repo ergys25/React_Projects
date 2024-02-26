5ae706377cd76b485ca818b799e15b32b167d706
Ergys Hila <ergysmania@gmail.com>
2024-02-25 22:23:13 -0500

    Section 3

--- Section 3/src/App.jsx
+++ Section 3/src/App.jsx
@@ -3,11 +3,14 @@ import {CORE_CONCEPTS} from './data';
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
-
+import {useState} from 'react'
+import {EXAMPLES} from './data';
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState('components');
+
     function handleSelect(selectedButton) {
-        console.log(selectedButton);
+        setSelectedTopic(selectedButton);
     }
 
     return (<div>
@@ -32,6 +35,16 @@ function App() {
                     <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                     <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                 </menu>
+                <div id="tab-content">
+                    <h3>{EXAMPLES[selectedTopic].title}</h3>
+                    <p>{EXAMPLES[selectedTopic].description}</p>
+                    <pre>
+                        <code>
+                            {EXAMPLES[selectedTopic].code}
+                        </code>
+                    </pre>
+
+                </div>
 
 
             </section>
